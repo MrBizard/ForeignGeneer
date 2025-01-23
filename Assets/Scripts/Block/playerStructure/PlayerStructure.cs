25144e56a0a3aeb414391fa767320df329393429
Nagiraud <nathangiraud123@gmail.com>
2025-01-23 23:25:41 +0100

    final

--- Assets/Scripts/Block/playerStructure/PlayerStructure.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ForeignGeneer.Assets.Scripts.block.playerStructure;
-
-public interface PlayerStructure
-{
-    public float pollutionInd { get; set; }
-    public void dismantle();
-    public void openUi();
-}
