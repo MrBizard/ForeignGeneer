4f1c95b47be9d4e9e3dd40567a2e243b97fb08c2
thrissian <thomas.rissian@gmail.com>
2025-03-17 09:20:16 +0100

    hotbar

---
+++ Assets/Scenes/UI/player/Hotbar.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class Hotbar : Panel
+{
+	List<SlotUI> slots = new List<SlotUI>();
+	public override void _Ready()
+	{
+		foreach(StackItem slot in InventoryManager.Instance.hotbar.slots)
+		{
+			
+		}
+	}
+}
